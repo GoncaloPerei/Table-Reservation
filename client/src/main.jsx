7ed0a1a1e9019e7fb1e5d85e2203e67bfc117b06
Gonçalo Pereira <83283563+GoncaloPerei@users.noreply.github.com>
2024-02-01 22:51:38 +0000

    Mudança Estrutura Ficheiro Main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,18 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Auth0Provider } from '@auth0/auth0-react';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Auth0Provider
       domain="dev-kzjm5760tstl1slt.us.auth0.com"
       clientId="qn7si2L0E2TJKsshQY6v26TZoWp3NIq2"
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: window.location.origin,
       }}
     >
       <App />
-    </Auth0Provider>,
-  </BrowserRouter>,
-)
+    </Auth0Provider>
+  </BrowserRouter>
+);
