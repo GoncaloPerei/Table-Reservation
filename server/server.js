f2d20e3daa2702ebe586333cc6b457ee6b006e3d
Gonçalo Pereira <83283563+GoncaloPerei@users.noreply.github.com>
2024-02-03 12:05:24 +0000

    feat: add cors options to backend server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const corsOptions = require("./src/config/corsOptions");
 
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 const db = require("./src/models");
 
 const userRouter = require("./src/routes/users");
-app.use("/users", userRouter)
+app.use("/users", userRouter);
 
 db.sequelize.sync().then(() => {
-    app.listen(5173, () => {
-        console.log("Server running on port 5173");
-    });
+  app.listen(5173, () => {
+    console.log("Server running on port 5173");
+  });
 });
