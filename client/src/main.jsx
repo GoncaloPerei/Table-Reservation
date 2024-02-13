e19ef4f383dbbb86c0db995300f826398b409ec9
Gonçalo Pereira <83283563+GoncaloPerei@users.noreply.github.com>
2024-02-13 20:44:35 +0000

    feat: add react strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,12 @@
-import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Auth0Provider
-      domain="dev-kzjm5760tstl1slt.us.auth0.com"
-      clientId="qn7si2L0E2TJKsshQY6v26TZoWp3NIq2"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </Auth0Provider>
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
