f65e7e410a362dce1bf765b074206e6519135321
Gonçalo Pereira <83283563+GoncaloPerei@users.noreply.github.com>
2024-02-18 16:55:26 +0000

    feat: add pagination buttons to menu on reserve page

--- client/src/pages/Reserve/layout/Menu/Menu.layout.jsx
+++ client/src/pages/Reserve/layout/Menu/Menu.layout.jsx
@@ -1,7 +1,8 @@
-import { Heading } from "../../components";
+import { Heading, ShowAllBTN } from "../../components";
 import useMealFetcher from "../../hooks/useMealFetcher.hook";
 import { MenuItem } from "../../components";
 import useVisibleState from "../../../../hooks/useVisibleState.hook";
+import ShowLessBTN from "../../components/Button/ShowLess.component";
 
 const Menu = () => {
   const { meals } = useMealFetcher();
@@ -20,19 +21,9 @@ const Menu = () => {
         ))}
       </ul>
       {visible < meals.length ? (
-        <button
-          onClick={showMoreItems}
-          className="tracking-wider hover:before:bg-blackborder-[#998650]-500 relative w-fit text-2xl text-[#998650] font-raleway font-light rounded-xl px-16 py-5 overflow-hidden border border-[#998650] bg-transparent transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-[#998650] before:transition-all before:rounded-xl before:duration-500 hover:text-white hover:rounded-xl hover:before:left-0 hover:before:w-full"
-        >
-          <span className="relative z-10">Show all...</span>
-        </button>
+        <ShowAllBTN onClick={showMoreItems} />
       ) : (
-        <button
-          onClick={showLessItems}
-          className="tracking-wider font-raleway hover:before:bg-blackborder-[#998650]-500 relative w-fit text-2xl text-[#998650] font-raleway font-light rounded-xl px-16 py-5 overflow-hidden border border-[#998650] bg-transparent transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-[#998650] before:transition-all before:rounded-xl before:duration-500 hover:text-white hover:rounded-xl hover:before:left-0 hover:before:w-full"
-        >
-          <span className="relative z-10">Show less...</span>
-        </button>
+        <ShowLessBTN onClick={showLessItems} />
       )}
     </div>
   );
