ac74cd933c71039c823bca828a6f6829e0660293
Gonçalo Pereira <83283563+GoncaloPerei@users.noreply.github.com>
2024-02-20 14:34:37 +0000

    feat: change loading animation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,41 +3,42 @@ import "./App.css";
 const HomePage = React.lazy(
   () =>
     new Promise((resolve) =>
-      setTimeout(() => resolve(import("./pages/Home/Home")), 1500)
+      setTimeout(() => resolve(import("./pages/Home/Home")), 1000)
     )
 );
 const ReservePage = React.lazy(
   () =>
     new Promise((resolve) =>
-      setTimeout(() => resolve(import("./pages/Reserve/Reserve")), 1500)
+      setTimeout(() => resolve(import("./pages/Reserve/Reserve")), 1000)
     )
 );
 const AboutPage = React.lazy(
   () =>
     new Promise((resolve) =>
-      setTimeout(() => resolve(import("./pages/About/About")), 1500)
+      setTimeout(() => resolve(import("./pages/About/About")), 1000)
     )
 );
 const ContactPage = React.lazy(
   () =>
     new Promise((resolve) =>
-      setTimeout(() => resolve(import("./pages/Contact/Contact")), 1500)
+      setTimeout(() => resolve(import("./pages/Contact/Contact")), 1000)
     )
 );
 const LoginPage = React.lazy(
   () =>
     new Promise((resolve) =>
-      setTimeout(() => resolve(import("./pages/Auth/Login/Login")), 1500)
+      setTimeout(() => resolve(import("./pages/Auth/Login/Login")), 1000)
     )
 );
 const RegisterPage = React.lazy(
   () =>
     new Promise((resolve) =>
-      setTimeout(() => resolve(import("./pages/Auth/Register/Register")), 1500)
+      setTimeout(() => resolve(import("./pages/Auth/Register/Register")), 1000)
     )
 );
+
 import { Routes, Route } from "react-router-dom";
-import { Loading } from "./components/index";
+import { Loading } from "./components";
 import { AuthContext } from "./context/Auth.context";
 import useAuthState from "./hooks/useAuthState.hook";
 
--- client/src/components/Loading/Loading.component.jsx
+++ client/src/components/Loading/Loading.component.jsx
@@ -1,9 +1,13 @@
-import DotLoader from "react-spinners/DotLoader";
+import Loader from "react-spinners/BeatLoader";
 
 const Loading = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-[#FBF751]">
-      <DotLoader size={100} color={"#000000"} />
+    <div
+      className={
+        "w-screen h-screen flex items-center justify-center bg-[#FBF751]"
+      }
+    >
+      <Loader size={30} color={"#000000"} />
     </div>
   );
 };
